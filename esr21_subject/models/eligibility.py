67bc9d7fe513ffa8934f39e777a15d65b365a6cd
Coulson Kgathi <ckgathi@gmail.com>
2021-05-26 07:37:52 +0200

    eligibility class

---
+++ esr21_subject/models/eligibility.py
@@ -0,0 +1,22 @@
+MIN_AGE_OF_CONSENT = 40
+MAX_AGE_OF_CONSENT = 60
+
+
+class Eligibility:
+
+    def __init__(self, age_in_years=None, **kwargs):
+        """checks if participant is eligible otherwise'
+        ' error message is the reason for'
+        ' eligibility test failed."""
+        self.error_message = []
+        self.age_in_years = age_in_years
+        if self.age_in_years < MIN_AGE_OF_CONSENT:
+            self.error_message.append(
+                'Participant is under {}'.format(MIN_AGE_OF_CONSENT))
+        if self.age_in_years > MAX_AGE_OF_CONSENT:
+            self.error_message.append(
+                'Participant is too old (>{})'.format(MAX_AGE_OF_CONSENT))
+        self.is_eligible = False if self.error_message else True
+
+    def __str__(self):
+        return "Screened, age ({})".format(self.age_in_years)
--- esr21_subject/models/eligibility_confirmation.py
+++ esr21_subject/models/eligibility_confirmation.py
@@ -6,6 +6,7 @@ from edc_identifier.model_mixins import NonUniqueSubjectIdentifierFieldMixin
 from edc_base.utils import get_utcnow
 
 from ..identifiers import ScreeningIdentifier
+from .eligibility import Eligibility
 
 
 class EligibilityConfirmation(NonUniqueSubjectIdentifierFieldMixin,
@@ -28,10 +29,28 @@ class EligibilityConfirmation(NonUniqueSubjectIdentifierFieldMixin,
         verbose_name='What is the participants age?',
         help_text='(Years)', )
 
+    ineligibility = models.TextField(
+        verbose_name="Reason not eligible",
+        max_length=150,
+        null=True,
+        editable=False)
+
+    is_eligible = models.BooleanField(
+        default=False,
+        editable=False)
+
+    # is updated via signal once subject is consented
+    is_consented = models.BooleanField(
+        default=False,
+        editable=False)
+
     def __str__(self):
         return self.screening_identifier
 
     def save(self, *args, **kwargs):
+        eligibility_criteria = Eligibility(self.age_in_years)
+        self.is_eligible = eligibility_criteria.is_eligible
+        self.ineligibility = eligibility_criteria.error_message
         if not self.screening_identifier:
             self.screening_identifier = self.identifier_cls().identifier
         super().save(*args, **kwargs)
