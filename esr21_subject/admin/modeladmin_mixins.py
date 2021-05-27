b5ec9feae75a18ef0a2d295fd16d23264b9e9f49
ame diphoko <diphokoame@gmail.com>
2021-05-27 20:56:49 +0200

    update eligibility confirmation model

---
+++ esr21_subject/admin/modeladmin_mixins.py
@@ -0,0 +1,25 @@
+from django_revision.modeladmin_mixin import ModelAdminRevisionMixin
+from edc_base.sites.admin import ModelAdminSiteMixin
+from edc_metadata import NextFormGetter
+from edc_model_admin import (
+    ModelAdminNextUrlRedirectMixin, ModelAdminFormInstructionsMixin,
+    ModelAdminFormAutoNumberMixin, ModelAdminAuditFieldsMixin,
+    ModelAdminReadOnlyMixin, ModelAdminInstitutionMixin,
+    ModelAdminRedirectOnDeleteMixin)
+
+from .exportaction_mixin import ExportActionMixin
+
+
+class ModelAdminMixin(ModelAdminNextUrlRedirectMixin,
+                      ModelAdminFormInstructionsMixin,
+                      ModelAdminFormAutoNumberMixin, ModelAdminRevisionMixin,
+                      ModelAdminAuditFieldsMixin, ModelAdminReadOnlyMixin,
+                      ModelAdminInstitutionMixin,
+                      ModelAdminRedirectOnDeleteMixin,
+                      ModelAdminSiteMixin,
+                      ExportActionMixin):
+
+    list_per_page = 10
+    date_hierarchy = 'modified'
+    empty_value_display = '-'
+    next_form_getter_cls = NextFormGetter
