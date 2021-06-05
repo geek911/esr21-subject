0b8f005b056fcf5d5946d8ab8747f6bd8602d45f
tumiemosweu <imosweuk@gmail.com>
2021-06-05 17:09:49 +0200

    update visit validations

--- esr21_subject/forms/subject_visit_form.py
+++ esr21_subject/forms/subject_visit_form.py
@@ -1,9 +1,9 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from edc_base.sites import SiteModelFormMixin
 from edc_constants.constants import OTHER
+from edc_visit_tracking.constants import LOST_VISIT, MISSED_VISIT, UNSCHEDULED
 from edc_form_validators import FormValidatorMixin
-from edc_form_validators.base_form_validator import INVALID_ERROR
-from edc_visit_tracking.constants import UNSCHEDULED, MISSED_VISIT
 from edc_visit_tracking.form_validators import VisitFormValidator as BaseVisitFormValidator
 
 from ..models import SubjectVisit
@@ -11,15 +11,20 @@ from ..models import SubjectVisit
 
 class VisitFormValidator(BaseVisitFormValidator):
 
-    def validate_visit_code_sequence_and_reason(self):
-        appointment = self.cleaned_data.get('appointment')
+    def validate_reason_and_info_source(self):
+
         reason = self.cleaned_data.get('reason')
-        if appointment:
-            if (appointment.visit_code not in ['1000']
-                    and reason == UNSCHEDULED):
-                raise forms.ValidationError({
-                    'reason': 'Invalid. This is not an unscheduled visit'},
-                    code=INVALID_ERROR)
+
+        if (reason == LOST_VISIT and
+                self.cleaned_data.get('info_source') in ['clinic_visit_w_subject',
+                                                         'other_contact_w_subject']):
+            msg = {'info_source': 'Source of information cannot be other contact with '
+                   'participant if participant has been lost to follow up.'}
+            self._errors.update(msg)
+            raise ValidationError(msg)
+
+    def validate_presence(self):
+        pass
 
     def validate_required_fields(self):
 
