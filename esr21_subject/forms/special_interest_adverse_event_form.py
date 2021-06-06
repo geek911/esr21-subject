caf87eb5d663135d96bd76a4ce1e07b75475d3f0
ame diphoko <diphokoame@gmail.com>
2021-06-06 16:20:34 +0200

    update aesi form validator cls

--- esr21_subject/forms/special_interest_adverse_event_form.py
+++ esr21_subject/forms/special_interest_adverse_event_form.py
@@ -1,10 +1,13 @@
 from django import forms
+from esr21_subject_validation.form_validators import SpecialInterestAdverseEventFormValidator
 
 from .form_mixins import SubjectModelFormMixin
 from ..models import SpecialInterestAdverseEvent
 
 
 class SpecialInterestAdverseEventForm(SubjectModelFormMixin, forms.ModelForm):
+
+    form_validator_cls = SpecialInterestAdverseEventFormValidator
 
     def has_changed(self):
         return True
