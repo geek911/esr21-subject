a51b071da9eab116c547658a7282d45333adc89d
Moses Chawawa <moseschawawa@gmail.com>
2021-06-24 14:20:19 +0200

    Lost to follow-up (use only when taking subject off study) to Use only when withdrawing subject off study

--- esr21_subject/choices.py
+++ esr21_subject/choices.py
@@ -345,7 +345,7 @@ VISIT_REASON = (
     (SCHEDULED, 'Scheduled visit/contact'),
     (MISSED_VISIT, 'Missed Scheduled visit'),
     (UNSCHEDULED, 'Unscheduled visit/contact'),
-    (LOST_VISIT, 'Lost to follow-up (use only when taking subject off study)'),
+    (LOST_VISIT, 'Use only when withdrawing subject off study'),
     (COMPLETED_PROTOCOL_VISIT, 'Subject has completed the study'),
 )
 
