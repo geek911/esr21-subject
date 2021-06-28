e1012c1e7014e84da61f5a9806c9998a7121314d
tumiemosweu <imosweuk@gmail.com>
2021-06-28 10:54:18 +0200

    update symptomatic infections model

--- esr21_subject/models/covid19_symptomatic_infections.py
+++ esr21_subject/models/covid19_symptomatic_infections.py
@@ -12,28 +12,33 @@ class Covid19SymptomaticInfections(CrfModelMixin):
         verbose_name='Has the participant experienced any symptomatic'
                      ' infection(s)?',
         max_length=20,
-        choices=YES_NO, )
+        choices=YES_NO,)
 
     symptomatic_infections = models.ManyToManyField(
         SymptomaticInfections,
-        verbose_name='Symptomatic infections '
+        verbose_name='Symptomatic infections ',
+        blank=True
     )
 
     date_of_infection = models.DateField(
-        verbose_name='Date of symptomatic infection (DD MMM YYYY):')
+        verbose_name='Date of symptomatic infection (DD MMM YYYY):',
+        null=True,
+        blank=True)
 
     infection_status = models.CharField(
         verbose_name='Previous SARS coV-2 infection status?',
         max_length=20,
-        choices=INFECTION_STATUS, )
+        choices=INFECTION_STATUS,)
 
     visits = models.CharField(
         verbose_name='Any hospital/ inpatient/ emergency room visit?',
         max_length=20,
-        choices=YES_NO, )
+        choices=YES_NO,)
 
     hospitalisation_date = models.DateField(
-        verbose_name='Date of hospitalisation:')
+        verbose_name='Date of hospitalisation:',
+        blank=True,
+        null=True,)
 
     class Meta(CrfModelMixin.Meta):
         app_label = 'esr21_subject'
