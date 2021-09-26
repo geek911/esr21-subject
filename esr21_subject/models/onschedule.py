7db765f8718e55e0987e0c6c3d3b29dec55ad939
Itumeleng Mosweu <imosweuk@gmail.com>
2021-09-26 15:19:27 +0200

    update onschedule model manager

--- esr21_subject/models/onschedule.py
+++ esr21_subject/models/onschedule.py
@@ -52,9 +52,9 @@ class OnScheduleIll(
                                      blank=True,
                                      null=True)
 
-    onsite = CurrentSiteManager()
-
     objects = SubjectIdentifierManager()
+
+    onsite = CurrentSiteManager()
 
     history = HistoricalRecords()
 
