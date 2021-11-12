1145e4b00823b88311c3b06593c64854c2e5b7ad
MoffatMore <mofenyimoffat@gmail.com>
2021-11-12 11:07:45 +0200

    remove signal for putting subject on schedule after second screening

--- esr21_subject/models/signals.py
+++ esr21_subject/models/signals.py
@@ -34,27 +34,27 @@ def metadata_update_on_post_save(sender, instance, raw, created, using,
                 instance.adverse_event.run_metadata_rules_for_crf()
 
 
-@receiver(post_save, weak=False, sender=ScreeningEligibility,
-        dispatch_uid='screening_eligibility_on_post_save')
-def screening_eligibility_on_post_save(sender, instance, raw, created, **kwargs):
-    """ Put participant on schedule post consent """
-    if not raw:
+# @receiver(post_save, weak=False, sender=ScreeningEligibility,
+#         dispatch_uid='screening_eligibility_on_post_save')
+# def screening_eligibility_on_post_save(sender, instance, raw, created, **kwargs):
+#     """ Put participant on schedule post consent """
+#     if not raw:
 
-        # if is_subcohort_full():
-        cohort = 'esr21'
-        # else:
-        # cohort = 'esr21_sub'
+#         # if is_subcohort_full():
+#         cohort = 'esr21'
+#         # else:
+#         # cohort = 'esr21_sub'
 
-        onschedule_model = 'esr21_subject.onschedule'
+#         onschedule_model = 'esr21_subject.onschedule'
 
-        if instance.is_eligible:
-            put_on_schedule(f'{cohort}_enrol_schedule', instance=instance,
-                            onschedule_model=onschedule_model,
-                            onschedule_datetime=instance.created.replace(microsecond=0))
+#         if instance.is_eligible:
+#             put_on_schedule(f'{cohort}_enrol_schedule', instance=instance,
+#                             onschedule_model=onschedule_model,
+#                             onschedule_datetime=instance.created.replace(microsecond=0))
 
-            put_on_schedule(f'{cohort}_fu_schedule', instance=instance,
-                            onschedule_model=onschedule_model,
-                            onschedule_datetime=instance.created.replace(microsecond=0))
+#             put_on_schedule(f'{cohort}_fu_schedule', instance=instance,
+#                             onschedule_model=onschedule_model,
+#                             onschedule_datetime=instance.created.replace(microsecond=0))
 
 # @receiver(post_save, weak=False, sender=Covid19SymptomaticInfections,
         # dispatch_uid='covid19_symptomatic_infections_on_post_save')
@@ -113,7 +113,6 @@ def appointment_on_post_save(sender, instance, raw, created, **kwargs):
 def put_on_schedule(schedule_name, onschedule_model, instance=None, onschedule_datetime=None):
 
     if instance:
-
         _, schedule = site_visit_schedules.get_by_onschedule_model_schedule_name(
             onschedule_model=onschedule_model, name=schedule_name)
 
