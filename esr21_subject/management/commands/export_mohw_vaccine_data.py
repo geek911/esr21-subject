6defb1bee8a3271eacca33fca443e7df92ad52d1
ame diphoko <diphokoame@gmail.com>
2021-11-26 10:03:34 +0200

    update mohw export, add extra fields

--- esr21_subject/management/commands/export_mohw_vaccine_data.py
+++ esr21_subject/management/commands/export_mohw_vaccine_data.py
@@ -10,38 +10,39 @@ class Command(BaseCommand):
     help = 'Export vaccine data'
 
     def handle(self, *args, **kwargs):
-
-        vaccinations = VaccinationDetails.objects.filter(received_dose='Yes')
-        exclude_fields = [
-            'created', 'modified', 'user_created', 'user_modified',
-            'hostname_created', 'hostname_modified', 'revision', 'device_created',
-            'device_modified', 'id', 'site_id', 'consent_model',
-            'consent_version', 'report_datetime']
+        vaccinations_tuple = ('received_dose_before', 'vaccination_site',
+                              'vaccination_date', 'lot_number', 'expiry_date',
+                              'provider_name')
+        vaccinations = VaccinationDetails.objects.filter(received_dose='Yes').only(
+            *vaccinations_tuple)
         count = 0
         toCSV = []
         for vaccination in vaccinations:
             obj_dict = vaccination.__dict__
 
-            for key in exclude_fields:
-                del obj_dict[key]
             consent = InformedConsent.objects.filter(
                 subject_identifier=vaccination.subject_visit.subject_identifier).last()
             first_name = consent.first_name
             last_name = consent.last_name
             dob = consent.dob
+            gender = consent.gender
+            age = consent.formatted_age_at_consent
+            site = consent.site.name
             country = None
             employment_status = None
+            employment_status_other = None
             subject_cell = None
-            
+
             identity_number = consent.identity
             identity_type = consent.identity_type
-            
+
             demographics = DemographicsData.objects.filter(
                 subject_visit__subject_identifier=vaccination.subject_visit.subject_identifier).last()
             if demographics:
                 country = demographics.country
                 employment_status = demographics.employment_status
-            
+                employment_status_other = demographics.employment_status_other
+
             try:
                 personal_contact = PersonalContactInfo.objects.get(
                     subject_identifier=vaccination.subject_visit.subject_identifier)
@@ -49,18 +50,25 @@ class Command(BaseCommand):
                 pass
             else:
                 subject_cell = personal_contact.subject_cell
+                physical_address = personal_contact.physical_address
             obj_dict.update(
                 first_name=first_name,
                 last_name=last_name,
                 dob=dob,
+                gender=gender,
+                age=age,
                 country=country,
                 employment_status=employment_status,
+                employment_status_other=employment_status_other,
                 subject_cell=subject_cell,
                 identity_number=identity_number,
-                identity_type=identity_type
+                identity_type=identity_type,
+                covidzone=site,
+                physical_address=physical_address
                 )
+            obj_dict.pop('id')
             obj_dict.pop('_state')
-            obj_dict.pop('form_as_json')
+#             obj_dict.pop('form_as_json')
             obj_dict.pop('subject_visit_id')
             toCSV.append(obj_dict)
             count += 1
